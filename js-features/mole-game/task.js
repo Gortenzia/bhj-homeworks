1aae00b79e5ffe92ca6617a81db83045e0e12966
Gortenzia <tacumineka@gmail.com>
2021-09-24 14:25:18 +0300

    1+ 2е и 3е задание с ошибками

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,8 @@
+const winCounter = document.getElementById('dead');
+const loozeCounter = document.getElementById('lost');
+const holeGame = document.getElementsByClassName('hole-game');
+
+for (let hole of holeGame) {
+    hole = getHole = index => document.getElementById(`hole${index}`),
+        hole.classList.contains('hole_has-mole') ? winCounter.textContent += 1 : loozeCounter.textContent += 1;
+}//undefined
