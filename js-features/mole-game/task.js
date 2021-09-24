31145a26ec1913f04b07dce55808792e2fd3b231
Gortenzia <tacumineka@gmail.com>
2021-09-24 14:28:59 +0300

    Исправление alert

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -6,3 +6,6 @@ for (let hole of holeGame) {
     hole = getHole = index => document.getElementById(`hole${index}`),
         hole.classList.contains('hole_has-mole') ? winCounter.textContent += 1 : loozeCounter.textContent += 1;
 }//undefined
+
+if (winCounter.textContent === 10) alert('Победа!');
+if (loozeCounter.textContent === 5) alert('Вы проиграли!');
