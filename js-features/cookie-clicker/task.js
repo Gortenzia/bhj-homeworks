c56bca6733393f1787d53da572266d520284ed06
Gortenzia <tacumineka@gmail.com>
2021-09-26 17:12:55 +0300

    3я задача пока считает не верно

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,8 @@
-const textCounter = document.getElementById("clicker__counter");
 const cookie = document.getElementById("cookie");
 
-
 cookie.onclick = function changeSizes() {
-    cookie.width = 200 ? cookie.width = 300 : cookie.width = 200;
-    textCounter.textContent = + 1;
-
-    //каждый следующий клик должен уменьшать ширину/высоту , но при этом прибавлять счетчик
-    //Срабатывает лишь один раз.
+    cookie.width === 200 ? cookie.width = 300 : cookie.width = 200;
+    const textCounter = document.getElementById("clicker__counter");
+    textCounter.textContent = Number(textCounter.textContent) + 1;
 }
 
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,11 +1,20 @@
 const winCounter = document.getElementById('dead');
 const loozeCounter = document.getElementById('lost');
-const holeGame = document.getElementsByClassName('hole-game');
 
-for (let hole of holeGame) {
-    hole = getHole = index => document.getElementById(`hole${index}`),
-        hole.classList.contains('hole_has-mole') ? winCounter.textContent += 1 : loozeCounter.textContent += 1;
-}//undefined
 
-if (winCounter.textContent === 10) alert('Победа!');
-if (loozeCounter.textContent === 5) alert('Вы проиграли!');
+for (let i = 1; i < 10; i++) {
+    hole = document.getElementById(String('hole' + i));
+    hole.onclick = function () {
+        if (hole.classList.contains('hole_has-mole')) {
+            winCounter.textContent = Number(winCounter.textContent) + 1;
+            if (winCounter.textContent === 10) alert('Победа!');
+            if (winCounter.textContent === 10) winCounter.textContent = 0;
+
+        } else {
+            loozeCounter.textContent = Number(loozeCounter.textContent) + 1;
+            if (loozeCounter.textContent === 5) alert('Вы проиграли!');
+            if (loozeCounter.textContent === 5) loozeCounter.textContent = 0;
+        }
+    }
+}
+//Работает не верно.
