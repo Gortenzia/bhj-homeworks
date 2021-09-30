23d70f9a752b14df71480d93c5e310d7cff6a1bb
Gortenzia <tacumineka@gmail.com>
2021-09-30 14:49:18 +0300

    ver 0.1

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,34 +1,25 @@
-function menuLinks() {
-    const menuMain = document.querySelectorAll('ul.menu_main');
+const ready = () => {
+    const menus = document.getElementsByClassName('menu_main');
 
-    for (let item of menuMain) {
-        const menuSub = item.querySelectorAll('ul.menu_sub');
-        const mainLink = item.querySelectorAll('li.menu__item');
-        const linkArr = Array.from(mainLink);
-        linkArr[0].onclick = function () {
-            return false;
-        }
-        linkArr[3].onclick = function () {
-            return false;
-        }
+    for (let menu of menus) {
+        const subMenus = menu.getElementsByClassName('menu_sub');
 
-        for (let sub of menuSub) {
-            const subLink = sub.closest('li.menu__item').querySelector('a.menu__link');
-
-
-            subLink.onclick = () => {
-                if (sub.classList.contains('menu_active')) {
-                    sub.classList.remove('menu_active');
+        for (let subMenu of subMenus) {
+            const link = subMenu.closest('.menu__item').querySelector('.menu__link');
+            link.onclick = () => {
+                if (subMenu.classList.contains('menu_active')) {
+                    subMenu.classList.remove('menu_active');
                     return false;
                 }
-                const menuActive = item.querySelector('.menu_active');
-                if (menuActive) {
-                    menuActive.classList.remove('menu_active');
+                const activeMenuItem = menu.querySelector('menu_active');
+                if (activeMenuItem) {
+                    activeMenuItem.classList.remove('menu_active');
                 }
-                sub.classList.add('menu_active');
+                subMenu.classList.add('menu_active');
                 return false;
-
-            }
+            };
         }
     }
-}
+};
+
+document.addEventListener('DOMContentLoaded', ready);
---
+++ element-search/slider/task.js
@@ -0,0 +1,34 @@
+function slider() {
+    const sliderItem = document.querySelectorAll('.slider__item');
+    const rightArrow = document.querySelector('.slider__arrow_next')
+    const leftArrow = document.querySelector('.slider__arrow_prev');
+
+    let index = 0;
+
+    const activeSlider = () => {
+        for (let slide of sliderItem) {
+            slide.classList.remove('slider__dot_active')
+        }
+        sliderItem.classList.add('slider__dot_active');
+    };
+
+    const next = () => {
+        (index === sliderItem.length - 1) ? index = 0 : (index += 1);
+        activeSlider(index)
+    }
+
+    const prev = () => {
+        (index === 0) ? index = sliderItem.length - 1 : (index -= 1);
+        activeSlider(index)
+    };
+
+    rightArrow.onclick = () => {
+        next();
+        return false;
+    }
+
+    leftArrow.onclick = () => {
+        prev();
+        return false;
+    }
+};
