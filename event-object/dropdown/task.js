105348fc5eb8d82ed66e573810433065020015dd
Gortenzia <tacumineka@gmail.com>
2021-11-14 18:45:34 +0300

    dropdown готов на 90%

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,10 +1,28 @@
+//const dropdownOnclick = () => {
 const dropdowns = Array.from(document.getElementsByClassName('dropdown__value'));
+const dropdownItems = Array.from(document.getElementsByClassName('dropdown__item'));
+const lists = Array.from(document.getElementsByClassName('dropdown__list'));
 
 for (let dropdown of dropdowns) {
-    const dropdownActiveItem = dropdown.classList.contains('dropdown__item');
-    if (dropdownActiveItem) dropdownActiveItem.classList.toggle('dropdown__list_active');
+    dropdown.addEventListener('click', function down() {
+        for (let list of lists) {
+            list.classList.toggle('dropdown__list_active');
+            const links = document.getElementsByClassName('dropdown__link');
+
+            for (let dropdownItem of dropdownItems) {
+                dropdownItem.addEventListener('click', function changeText() {
+                    dropdown.textContent = dropdownItem.textContent;
+                    if (Event.currentTarget < links) Event.preventDefault();
+                })
+
+
+            }
+        }
+    })
 }
+//}
+//document.addEventListener('DOMContentLoaded', dropdownOnclick);
 
-
-//В элементе с классом dropdown__value содержится выбранное значение.
-//Для того, чтобы открыть меню, требуется проставить класс dropdown__list_active:
+ //EventTarget -
+ //event.currentTarget
+ //event.target - 
