13dbbceb2ad95460891c2c04e2aa9c20b2b63063
Gortenzia <tacumineka@gmail.com>
2021-11-29 22:52:03 +0300

    All done

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -8,39 +8,31 @@ let tabContentMain = tabContentElements[0];
 let tabContentAbout = tabContentElements[1];
 let tabContentContakts = tabContentElements[2];
 
-tabElementMain.onclick = function () {
-    check();
+tabElementMain.onclick = () => {
+    tabElementAbout.classList.remove('tab_active');
+    tabContentAbout.classList.remove('tab__content_active');
+    tabElementContakts.classList.remove('tab_active');
+    tabContentContakts.classList.remove('tab__content_active');
     tabElementMain.classList.add('tab_active');
     tabContentMain.classList.add('tab__content_active');
+
 }
 
-tabElementAbout.onclick = function () {
-    check();
+tabElementAbout.onclick = () => {
+    tabElementMain.classList.remove('tab_active');
+    tabContentMain.classList.remove('tab__content_active');
+    tabElementContakts.classList.remove('tab_active');
+    tabContentContakts.classList.remove('tab__content_active');
     tabElementAbout.classList.add('tab_active');
     tabContentAbout.classList.add('tab__content_active');
 }
 
-tabElementContakts.onclick = function () {
-    check();
+tabElementContakts.onclick = () => {
+    tabElementMain.classList.remove('tab_active');
+    tabContentMain.classList.remove('tab__content_active');
+    tabElementAbout.classList.remove('tab_active');
+    tabContentAbout.classList.remove('tab__content_active');
     tabElementContakts.classList.add('tab_active');
     tabContentContakts.classList.add('tab__content_active');
 }
 
-function check() {
-    if (tabElementMain.classList.contains('tab_active') || tabElementContakts.classList.contains('tab_active')) {
-        tabElementAbout.classList.remove('tab_active');
-        tabContentAbout.classList.remove('tab__content_active');
-    }
-
-    if (tabElementContakts.classList.contains('tab_active') || tabElementAbout.classList.contains('tab_active')) {
-        tabElementMain.classList.remove('tab_active');
-        tabContentMain.classList.remove('tab__content_active');
-    }
-
-    if (tabElementMain.classList.contains('tab_active') || tabElementAbout.classList.contains('tab_active')) {
-        tabElementContakts.classList.remove('tab_active');
-        tabContentContakts.classList.remove('tab__content_active');
-    }
-}
-
-//Иногда текстовое содержимое добавляется, вместо полной замены.
