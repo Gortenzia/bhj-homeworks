03c15902f9d46f0318a1c5bd421282df8957ab11
Gortenzia <tacumineka@gmail.com>
2021-12-06 12:34:25 +0300

    Исправлен цвет текста

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -4,20 +4,24 @@ const rotator = () => {
     rotatorCases[index].classList.remove('rotator__case_active');
     index += 1;
     rotatorCases[index].classList.add('rotator__case_active');
+    const color = rotatorCases[index].dataset.color;
+    rotatorCases[index].style.color = color;
+    const speed = rotatorCases[index].dataset.speed;
+    rotatorCases[index].style.speed = speed;
     if (index === rotatorCases.length - 1) index = 0;
 }
 setInterval(rotator, 1000);
 
 //Попытка сделать более сложную версию
-for (let rotatorCase of rotatorCases) {
-    for (let attr of rotatorCase.attributes) {
-        console.log(`${attr.name} = ${attr.value}`);
-        if (this.attr.dataset.color === 'red') {//Cannot read properties of undefined (reading 'color')
-            this.rotatorCase.style.color = 'red'; //Не верно, цвет не присваивает
-        }
-    }
-    let computedStyle = getComputedStyle(document.rotatorCase);
-    console.log(computedStyle.dataColor);
+//for (let rotatorCase of rotatorCases) {
+//for (let attr of rotatorCase.attributes) {
+// console.log(`${attr.name} = ${attr.value}`);
+// if (this.attr.dataset.color === 'red') {//Cannot read properties of undefined (reading 'color')
+//   this.rotatorCase.style.color = 'red'; //Не верно, цвет не присваивает
+//}
+// }
+//let computedStyle = getComputedStyle(document.rotatorCase);
+//console.log(computedStyle.dataColor);
 
-}
+//}
 document.addEventListener('DOMContentLoaded', rotator);
